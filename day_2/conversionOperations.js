a05b5c9b6652fd7549a067261ffbf4beef7bad1a
Ashwani <64095941+harjai16@users.noreply.github.com>
2024-06-27 01:29:04 +0000

    day 2 learning of js

---
+++ day_2/conversionOperations.js
@@ -0,0 +1,20 @@
+let score = null
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33fjak" => nan
+// "true" => 1, "false" => 0
+
+let isLoggedIn = 4
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
